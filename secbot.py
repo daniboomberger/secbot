f8a169f8b68af0164aa755b55d339afff6bc9fa7
din jakupi <din.jakupi@gmail.com>
2020-06-12 01:11:06 +0200

    checks url via virustotal api and shows the result of Avira as discord message

--- secbot.py
+++ secbot.py
@@ -1,24 +1,33 @@
 from discord.ext import commands
+import requests
 import discord
 import re
 import credentials
 
-client = discord.Client()
 
+url = 'https://www.virustotal.com/vtapi/v2/url/report'
+discordClient = discord.Client()
+
+dictionary = dict()
 regex = re.compile(
     r'^(?:http|ftp)s?://' # http:// or https://
-    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
-    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ip
     r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
-@client.event
+@discordClient.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(discordClient))
 
-@client.event
+@discordClient.event
 async def on_message(message):
-    isUrl = re.match(regex, message.content)
+    targetUrl = message.content
+    isUrl = re.match(regex, targetUrl)
     if isUrl:
-        await message.channel.send('url was sent!')
+        params = {'apikey': credentials.virusTotalToken, 'resource': targetUrl}
+        resp = requests.get(url, params)
+        data = resp.json()
+        result = "url: {0}, timestamp: {1}, result: {2}".format(data['url'], data['scan_date'], data['scans']['Avira']['result'])
+        await message.channel.send(result)
 
-client.run(credentials.TOKEN)
+discordClient.run(credentials.discordToken)
